3fda4daf91cda28e9843779fd55a72d647a1db25
Natyy__ <131829677+natyy200@users.noreply.github.com>
2025-02-18 21:21:25 -0300

    Add files via upload

---
+++ Scripts/0. criar_esquemas.sql
@@ -0,0 +1,18 @@
+# Criando um esquema (uma base de dados)
+# Create: criar esquemas, tabelas tipos, dominios, views, triggers entre outros..
+# Esquema: Entidade que agrupa tabelas pertecentes a mesma aplicação
+
+# 1. identificar se o banco de dados está no ar.
+# 2. MYSQL Workbench e clicar na conexão criada na preparação do ambiente
+# 3. Clicar em "Schema" na parte de baixo de "Navigator"
+
+CREATE SCHEMA CLUBE_DO_LIVRO;
+
+
+# 4. Seleciona o comando
+# 5. Apertar no raiozinho para executar o código
+# 6. E atualizar a visualização no botão de setinhas em "Schema" na parte de baixo de "Navigator"
+
+
+
+
